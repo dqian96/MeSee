90ab13f0a5a1826be78c6445c598383641281be4
Aamir Ahmad <aamirahmad@outlook.com>
2016-10-15 15:24:02 -0400

    Add Yelp Review average + env variables

--- routes/yelp.js
+++ routes/yelp.js
@@ -1,19 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var path = require("path");
+var Yelp = require('yelp');
+const dotenv = require('dotenv');
+
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
+dotenv.load({ path: 'env_vars' });
+
+var yelp = new Yelp({
+  consumer_key: process.env.YELP_CONSUMER_KEY,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET,
+  token: process.env.YELP_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET,
+}); 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/location/:location', function(req, res, next) {
+    var location = req.params.location;
+    getRatingsOfPlaces(location, function(err, data){
+        console.log(data);
+    }) 
+    res.render('index', { title: 'Express' });
 });
 
 function getRatingsOfPlaces(location, callback){
-    yelp.search({ location: location }, function(err, data){
+    yelp.search({ location: location , radius_filter : 1500}, function(err, data){
         if(err) throw err;
-        for(var i = 0; i < data.length; ++i){
-            
+        var average = 0, sum = 0;
+        for(var i = 0; i < data.businesses.length; ++i){
+            sum += data.businesses[i].rating;
         }
-        callback(null, data);	
+        average = sum / data.businesses.length;
+        callback(null, average);	
     })
 }
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-
+var path = require('path');
+var jade = require('jade');
 
 // Creating an app
 var app = express();
@@ -13,31 +14,25 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var routes = require('app/routes/index');
+var routes = require('./routes/index');
+var yelpRoute = require('./routes/yelp');
 app.use('/', routes);
+app.use('/yelp', yelpRoute);
 
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // Database
 mongoose.connect('mongodb://localhost:27017/ratemyhouse');
 var db = mongoose.connection;
-
-// Yelp API
-var Yelp = require('yelp');
-var yelp = new Yelp({
-  consumer_key: 'consumer-key',
-  consumer_secret: 'consumer-secret',
-  token: 'token',
-  token_secret: 'token-secret',
-});
-
+var port = 3000;
 
 // Server:
 // -app listens on 8080 for requests
 // -call back once instantiated
-var server = app.listen(8080, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("App listening at http://%s:%s", host, port)
+var server = app.listen(port, function () {
+   console.log("App listening at http://localhost/%s", port)
 })
 
 // export of module i.e. return
