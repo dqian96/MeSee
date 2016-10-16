31439ee11a2de8250f0189f5c6ba6c34deec079e
Aamir Ahmad <aamirahmad@outlook.com>
2016-10-15 21:39:21 -0400

    Add Indico Sentiment Analysis

--- routes/yelp.js
+++ routes/yelp.js
@@ -4,7 +4,8 @@ var path = require("path");
 var Yelp = require('yelp');
 var indico = require('indico.io');
 
-indico.apiKey = 'f844248f8582881cdf65b74c1718d0ac';
+//indico.apiKey = process.env.INDICO_API_KEY;
+indico.apiKey = '61f321e617349d9852cef5d2424baa96';
 
 
 var yelp = new Yelp({
@@ -33,15 +34,29 @@ router.get('/location1/:location', function(req, res, next) {
     res.render('index'); 
 });
 
+router.get('/location2/:location', function(req, res, next) {
+    var location = req.params.location;
+    indicoBatchSentimentAnalysis(location, function(err, data){
+        console.log(data);
+        res.json(data);
+    })
+    res.render('index'); 
+});
+
+
+// RATINGS FUNCTION
 function getRatingsOfPlaces(location, callback){
     yelp.search({ location: location , radius_filter : 1500}, function(err, data){
         if(err) throw err;
         var average = 0, sum = 0;
         for(var i = 0; i < data.businesses.length; ++i){
             sum += data.businesses[i].rating;
+            console.log(data.businesses[i].rating);
         }
         average = sum / data.businesses.length;
-        callback(null, data);	
+        average = (average / 5) * 70;
+        average = Number((average).toFixed(1));
+        callback(null, average);	
     })
 }
 
@@ -63,29 +78,40 @@ function getReviewsOfNearbyPlaces(location, callback){
     getPlacesInArea(location, function(err, data){
         if(err) throw err;
         var arr = new Array;
-        
         var x  = 0;
         for(var i = 0; i < data.businesses.length; i++){
             var business_id = data.businesses[i].id;
             getBusinessesInArea(business_id, function(err, business){
                 if (err) throw err;
-                console.log(business);
                 var obj = {id : business.id, review : business.reviews[0].excerpt};
                 arr.push(obj);
-                if(++x == data.businesses.length) callback(null, arr);
+                if(++x == data.businesses.length){
+                    var reviewArr = arr.map(function(o){return o.review;}); 
+                    callback(null, reviewArr);
+                }
             })
         };
-        //callback(null, arr);
     })
 }
 
-function indicoBatchSentimentAnalysis(reviewArr, callback){
-    indico.sentiment(reviewArr, function(err, results){
-        if (err) throw err;
-        var total = 0;
-        for(var i in arr) { total += arr[i]; }
-        var avg = total / reviewArr.length;
-        callback(err, avg);
+// SENTIMENT FUNCTION
+// ask mentor about promises shit
+function indicoBatchSentimentAnalysis(location, callback){
+    getReviewsOfNearbyPlaces(location, function(err, reviewArr){
+        indico.sentiment(reviewArr)
+        .then(function(res) {
+            var total = 0;
+            for(var i in res) { total += res[i]; }
+            var avg = 0;
+            avg = total / res.length;
+            avg *= 5;
+            avg = Number((avg).toFixed(1));
+            //return avg;
+            callback(null, avg);
+        })
+        .catch(function(err) {
+            //if (err)  throw err;
+        });
     })
 }
 
@@ -94,3 +120,4 @@ function consolidateTotal(ratings, indico, crime, callback){
     // generate a final score
 }
 module.exports = router;
+ 
